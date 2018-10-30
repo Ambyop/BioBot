203cb5733e795332d4bcc3a9cee91000849d03fd
ambio98 <44583683+ambio98@users.noreply.github.com>
2018-10-30 08:47:33 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -27,21 +27,48 @@ bot.on('message', message=>{
     if (command === '!blah') {
         message.channel.send('Meh.');
     }
+    var dateStop = false;
     if (command === "!date") {
         let jour = args[0]; // jour
         let mois = args[1]; //date
         let annee = args[2]; // mois
-        var date = new Date(annee,mois-1,jour);
-        message.channel.send(`${message.author.username}, lancement de la boucle à la date du ${jour} / ${mois} /${annee} `);
-        var interval = setInterval (function () {
-            date.setDate(date.getDate()+2);// ajout de 2jours
-            message.channel.send("On passe au jour" +date);
-        }, 3600 * 1000); // 1heure = 3600 secondes
 
+        message.reply('La commande a été désactivée pour le moment \n https://www.tenor.co/t20G.gif ');
+        /*var intervalle = function () {
+            if (dateStop === false) {
+                date.setDate(date.getDate() + 2);// ajout de 2jours
+                message.channel.send("On passe au jour : " + date);
+                setTimeout(intervalle, 3 * 1000);
+            }
+            else{
+                clearTimeout(intervalle);
+                interval = undefined;
+            }
+        };
+
+
+        if ((jour === undefined || mois === undefined || annee === undefined )&& message.content !=='!date stop') {
+            message.reply('Vous avez oublié d\'encoder l\'année de départ :wink: \n !date [dd] [mm] [yyyy)');
+        }
+        /*else if(message.content ==='!date stop'){
+            message.channel.send('La boucle a été arrêtée :wink:');
+            //clearInterval(intervalle);
+            dateStop = true;
+        }
+        else {
+                var date = new Date(annee, mois - 1, jour);
+                message.channel.send(`${message.author.username}, lancement de la boucle à la date du ${jour} / ${mois} /${annee} `);
+                setTimeout(intervalle, 3 * 1000); // 1heure = 3600 secondes
+
+        }*/
 
     }
+    if (message.content ==='!datestop'){
+        message.channel.send('La boucle a été arrêtée :wink:');
+        dateStop = true;
+    }
     if (message.content ==='!help date'){
-        message.channel.send('Voici le format de la commande:\n !date [dd] [mm] [yyyy]')
+        message.channel.send('Voici le format de la commande:\n !date [dd] [mm] [yyyy] \n Pour l\'arrêter il suffit de taper !datestop');
     }
     if (message.content ==='!coco'){
         message.reply('https://i.imgur.com/0TWyD8S.gif')
@@ -49,6 +76,7 @@ bot.on('message', message=>{
     if (message.content ==='!help coco'){
         message.channel.send('Envoie un Gif de Staline')
     }
+
     
 });
 
