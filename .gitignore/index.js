5f54212ed864589b69b894f73c4f8b5d0fbee0c4
ambio98 <44583683+ambio98@users.noreply.github.com>
2018-11-07 17:05:40 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,7 @@ const { prefix, token } = require('./config.json');
 bot.on("ready",function() {
     bot.user.setGame(`${prefix}help [commande]`);
     console.log("Bot connecté");
-    bot.user.setStatus('online') // online, idle, dnd, invisible
+    bot.user.setStatus('dnd') // online, idle, dnd, invisible
 });
 
 
@@ -141,7 +141,7 @@ bot.on('message', message=> {
     }
     if (command ===`${prefix}nazi`){
         let gif=['https://tenor.com/view/power-rangers-pose-squad-goals-salute-nazi-salute-gif-3535967','https://tenor.com/view/hitler-nazi-gif-7618295','https://tenor.com/view/hitler-dance-gif-4821571','https://media.giphy.com/media/wSpM9vIYEvGV2/giphy.gif','https://i.pinimg.com/originals/35/98/fb/3598fb2eb8a799cbcd970788b69f87f6.gif',
-        'https://i.imgur.com/qLBFR.gif'];
+        'https://i.imgur.com/qLBFR.gif','https://risibank.fr/cache/stickers/d548/54802-full.gif','https://static.comicvine.com/uploads/original/11133/111330552/6006006-6211688823-giphy.gif','https://www.brain-magazine.fr/m/posts/19551/max/KKKnazi2.gif'];
         if (args[0] === undefined) {
             let valeur = nombreAleatoire(gif.length);
             message.channel.send(gif[valeur - 1])
@@ -162,7 +162,8 @@ bot.on('message', message=> {
     if (command ===`${prefix}trump`){
         let gif=["https://www.tenor.co/PgFD.gif","https://www.tenor.co/yiQN.gif","https://www.tenor.co/HdtG.gif","https://giphy.com/gifs/someone-run-shooter-iBcLqvp8FMwy3AiPGY ","https://www.tenor.co/GnfE.gif ",
         "https://orig00.deviantart.net/715a/f/2015/318/9/7/_gif__donald_trump_blasts_off_by_jaders75-d9gqh7w.gif","https://media.giphy.com/media/3o7TKwiaIuMib5WVXO/giphy.gif","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLtSA1rCkgEVgNNazRh60NB9SIjweByni-f1K2bp8t1ROm5kxg",
-        "http://wanna-joke.com/wp-content/uploads/2016/06/trump-gif-belgium-city.gif"];
+        "http://wanna-joke.com/wp-content/uploads/2016/06/trump-gif-belgium-city.gif","https://cdn.fbsbx.com/v/t59.2708-21/36082833_474759779634569_1676559523583623168_n.gif?_nc_cat=101&_nc_ht=cdn.fbsbx.com&oh=4dbbd9ab01a6239b26da33dc13c913f7&oe=5BE4DB7E",
+        'https://1.bp.blogspot.com/-JD7_vfnxFjY/WYIZTlr8IoI/AAAAAAAA9nY/7sGP6-OvcCofIwTHPt1gxzCz3qJ1xFZugCLcBGAs/s1600/rkM5X.gif','https://i.imgur.com/LAyeUCl.gif?noredirect','https://static1.squarespace.com/static/56a16fd5d82d5ee027b1b53f/t/5931b8a815cf7da7c1cb767b/1496430802427/?format=500w'];
         if (args[0] === undefined) {
             let valeur = nombreAleatoire(gif.length);
             message.channel.send(gif[valeur - 1])
@@ -204,7 +205,8 @@ bot.on('message', message=> {
         let gif=['https://giphy.com/gifs/next-pFwRzOLfuGHok','https://www.tenor.co/PXLU.gif','https://www.tenor.co/V2dm.gif','https://www.tenor.co/HLUY.gif',
         'https://giphy.com/gifs/funny-cute-lol-26gsspfbt1HfVQ9va','https://giphy.com/gifs/monkey-12uB4fsiMsC8V2','https://www.tenor.co/xkQv.gif','https://www.tenor.co/JM5S.gif','https://www.tenor.co/x19S.gif',
         'http://humourtop.com/gifs-animes-droles-de-singes/Bebe_Chimpanze.gif','http://humourtop.com/gifs-animes-droles-de-singes/Humour_Gorille.gif','https://thumbs.gfycat.com/ReflectingTangibleBlueandgoldmackaw-small.gif',
-        'https://media.giphy.com/media/IKv6damwRCkFy/giphy.gif','https://s.yimg.com/ny/api/res/1.2/1Y839XJpE2V77BmClZa2wg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NDgwO2g9Mjcw/http://media.zenfs.com/fr_FR/News/Francetvinfo.fr/6292545.gif'];
+        'https://media.giphy.com/media/IKv6damwRCkFy/giphy.gif','https://s.yimg.com/ny/api/res/1.2/1Y839XJpE2V77BmClZa2wg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NDgwO2g9Mjcw/http://media.zenfs.com/fr_FR/News/Francetvinfo.fr/6292545.gif',
+        'http://gifdrole.com/singes/gorille_rock.gif'];
         if (args[0] === undefined) {
             let valeur = nombreAleatoire(gif.length);
             message.channel.send(gif[valeur - 1])
@@ -224,7 +226,9 @@ bot.on('message', message=> {
     }
     if (command ===`${prefix}popcorn` || command === `${prefix}pop`|| command === `${prefix}corn`){
         let gif=['https://i.pinimg.com/originals/6d/2f/19/6d2f1933311596e0ad7d349b7e7c2b6f.gif','https://media.giphy.com/media/tFK8urY6XHj2w/giphy.gif','https://media.giphy.com/media/128UMaujdjX7Pi/giphy.gif','https://media.giphy.com/media/12aW6JtfvUdcdO/giphy.gif','https://media.giphy.com/media/TrDxCdtmdluP6/giphy.gif','https://media.giphy.com/media/1pw5Hn77ylYxW/giphy.gif',
-        'https://giphy.com/gifs/reactiongifs-tyqcJoNjNv0Fq','https://giphy.com/gifs/chill-chihuahua-4XSc0NkhKJQhW','https://tenor.com/view/nom-eat-eating-snack-popcorn-gif-5369618','https://giphy.com/gifs/corn-Znj9tJamHmLoQ '];
+        'https://giphy.com/gifs/reactiongifs-tyqcJoNjNv0Fq','https://giphy.com/gifs/chill-chihuahua-4XSc0NkhKJQhW','https://tenor.com/view/nom-eat-eating-snack-popcorn-gif-5369618','https://giphy.com/gifs/corn-Znj9tJamHmLoQ ',
+        'https://media.giphy.com/media/pUeXcg80cO8I8/giphy.gif','https://media.giphy.com/media/l3q2FbmLvAockl43u/giphy.gif','https://media.giphy.com/media/UlqLDtI8Qc0j6/source.gif','https://media.giphy.com/media/2ALbeBfUZME4aCdyZg/giphy.gif',
+        'https://www.reactiongifs.us/wp-content/uploads/2017/12/popcorn.gif'];
         if (args[0] === undefined) {
             let valeur = nombreAleatoire(gif.length);
             message.channel.send(gif[valeur - 1])
@@ -281,7 +285,8 @@ bot.on('message', message=> {
         }
     }
     if (command ===`${prefix}kim`){
-        let gif=["https://media.giphy.com/media/2cmCUDzzxscnK/giphy.gif","https://media1.tenor.com/images/a02f0ac5e22ecba79aa36a9564b5d7ec/tenor.gif?itemid=9924807","https://66.media.tumblr.com/a7d6ec418cfca8c9b60b2c47e667f6e5/tumblr_nr6fptQVIA1s4bl2qo1_r5_400.gif","http://i.imgur.com/n2AIQ5y.gif"];
+        let gif=["https://media.giphy.com/media/2cmCUDzzxscnK/giphy.gif","https://media1.tenor.com/images/a02f0ac5e22ecba79aa36a9564b5d7ec/tenor.gif?itemid=9924807","https://66.media.tumblr.com/a7d6ec418cfca8c9b60b2c47e667f6e5/tumblr_nr6fptQVIA1s4bl2qo1_r5_400.gif","http://i.imgur.com/n2AIQ5y.gif",
+        'https://media.giphy.com/media/26xBOVNvTYppStULC/giphy.gif','https://i3.kym-cdn.com/photos/images/newsfeed/000/853/445/64b.gif'];
         if (args[0] === undefined) {
             let valeur = nombreAleatoire(gif.length);
             message.channel.send(gif[valeur - 1])
@@ -375,36 +380,39 @@ bot.on('message', message=> {
     const args = message.content.slice().trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    let arretBoucle=false
-    if (command === `${prefix}bouclep`) {
+    if (command === `${prefix}jour`) {
         let jour = args[0]; // jour
         let mois = args[1]; //date
         let annee = args[2]; // mois
-
-        var intervalle = function () {
-            if (!arretBoucle) {
-                date.setDate(date.getDate() + 1);// ajout de 1 jours
-                message.channel.send("On passe au jour : " + date);
-            }
-            else {
-                clearInterval(intervalle);
-                interval = undefined;
-            }
-        };
-
-        if ((jour === undefined || mois === undefined || annee === undefined) && message.content !== '!bouclep stop') {
-            message.reply('Vous avez oublié d\'encoder l\'année de départ :wink: \n !bouclep [dd] [mm] [yyyy)');
+        var date = new Date(annee, mois - 1, jour);
+        var compteur = "v2";
+        if(args[0] ==="stop" || args[0]==="fin"){
+            count=5;
+            compteur = "stop";
+            message.reply('fin')
         }
         else {
-            var date = new Date(annee, mois - 1, jour);
-            message.channel.send(`${message.author.username}, lancement de la boucle à la date du ${jour} / ${mois} /${annee} `);
-            setInterval(intervalle, 4* 3600 * 1000); // 1heure = 3600 secondes
-
+            message.channel.send(`**${message.author.username}** lancement de la boucle journée à partir du `+ args[0] +"/"+args[1]+"/"+args[2]);
+            var count = 0;
+            let temoin = 0;
+            var interval = setInterval(function () {
+                date.setDate(date.getDate() + 1);// ajout de 1 jours
+                message.channel.send("Nous passons au jour : " + date);
+                temoin++;
+                console.log(temoin + " " + count+ " "+ compteur);
+                if (count === 5) {
+                    console.log('exiting');
+                    clearInterval(interval);
+                }
+            }, 3 * 1000);
         }
     }
-});
 
-bot.login(process.env.TOKEN);
+
+    });
+
+
+bot.login("NTA2ODA1NzIwNjU5MDAxMzU2.Drn_Ng.EMz5foeHFtyW6VIbQbV-rOJjuHM");
 bot.on('error', console.error);
 
 function nombreAleatoire(nombre) {
